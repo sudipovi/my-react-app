b3a9501f4b6be41aff260811d377faf7ac84a086
sudipovi <shudipdey73@gmail.com>
2025-09-20 22:56:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Countries from "./components/Countries/countries";
+
+// http://localhost:5173/
+
+function App() {
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class countries extends Component {
+  render() {
+    return (
+      <div>
+        <h1>In the country</h1>
+      </div>
+    );
+  }
+}
+
+export default countries;
